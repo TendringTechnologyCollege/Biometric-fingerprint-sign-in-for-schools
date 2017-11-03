e2a3fbb76dea09116d4aefd7e135c33527efff5f
Callum Mason <clmason99@hotmail.co.uk>
2017-11-03 18:33:12 +0000

    GUI WOKING!!!!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,23 +2,67 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 
 import java.awt.*;
 
+
 public class Controller {
+
+    private String Username = "Hello";
+    private String Password = "World";
+
     @FXML private Text actiontarget;
     @FXML private TextField UserName;
+    //@FXML private TextField passwordField;
+    @FXML private TextField passwordField;
 
 
-    public void handleSubmitButtonAction(ActionEvent actionEvent) {
-        System.out.println(UserName.getText());
+    @FXML
+    public void onEnter(KeyEvent e){
+
+        if(e.getCode().equals(KeyCode.ENTER)){
+            Signin();
+        }
 
     }
 
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
 
-    public void UserNameTF(ActionEvent actionEvent) {
+        Signin();
+
 
     }
+
+    private void Signin() {
+
+
+
+        String username = UserName.getCharacters().toString();
+        String password = passwordField.getCharacters().toString();
+        System.out.println(username + ", " + password);
+
+        if (username.equals(Username)) {
+
+            if (password.equals(Password)) {
+
+                actiontarget.setText(username + ", " + password + " - Correct");
+                System.out.println("Correct");
+
+            }
+
+        } else {
+            System.out.println("Incorrect");
+            actiontarget.setText(username + ", " + password + " - Incorrect");
+
+
+
+        }
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,19 +11,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
 
 
         primaryStage.getIcons().add(new Image(Controller.class.getResourceAsStream("img/Icon.png")));
-
-
         primaryStage.setTitle("Welcome | Sign in");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 575));
+        primaryStage.setFullScreen(true);
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
